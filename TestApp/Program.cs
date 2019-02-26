3dae96cae36ba660670b6e5874e7a607b443a201
Максим Найдович <max.naid@mail.ru>
2019-02-26 22:47:29 +0500

    работа с временем, определения текущего UTC

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Text.RegularExpressions;
 
 namespace TestApp
 {
@@ -6,17 +10,42 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Print(GetSquare(42));
-        }
+            DateTime date = new DateTime();
+            Console.WriteLine("Zero time position: " + date + "\n");
 
-        private static void Print(object v)
-        {
-            Console.WriteLine(v);
-        }
+            date = DateTime.Now;
+            Console.WriteLine("Current time position: " + date + "\n");
 
-        private static int GetSquare(int v)
+            date = date.AddHours(3);
+            Console.WriteLine("Time in China now: " + date + "\n");
+
+           
+            Console.WriteLine("Current Greenwich Mean Time:" + date.ToUniversalTime() + "\n"); //похоже метод не знает о реальном времени по гринвичу
+
+            Console.WriteLine("Current UTC Time form nist:" + GetNistTime() + "\n");
+        }
+        //взял с https://stackoverflow.com/questions/6435099/how-to-get-datetime-from-the-internet
+        public static DateTime GetNistTime()
         {
-            return (int)Math.Pow(v, 2);
+            DateTime dateTime = DateTime.MinValue;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://nist.time.gov/actualtime.cgi?lzbc=siqm9b");
+            request.Method = "GET";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore); //No caching
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader stream = new StreamReader(response.GetResponseStream());
+                string html = stream.ReadToEnd();//<timestamp time=\"1395772696469995\" delay=\"1395772696469995\"/>
+                string time = Regex.Match(html, @"(?<=\btime="")[^""]*").Value;
+                double milliseconds = Convert.ToInt64(time) / 1000.0;
+                dateTime = new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+            }
+
+            return dateTime;
         }
     }
 }
